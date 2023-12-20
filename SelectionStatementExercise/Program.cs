b8f0a642b67de5c7243851c1475160a9c39f4b16
AdrienneUsi <usiadrienne35@gmail.com>
2023-12-19 20:29:10 -0800

    resubmit exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -5,10 +5,10 @@
         static void Main(string[] args)
         {
             var r = new Random();
-            var favNumber = r.Next(1, 10);
+            var favNumber = r.Next(1, 1000);
            
 
-            Console.WriteLine("Pick a number from 1 to 10");
+            Console.WriteLine("Pick a number from 1 to 1000");
             var userInput = int.Parse(Console.ReadLine());
 
             if(userInput < favNumber)
